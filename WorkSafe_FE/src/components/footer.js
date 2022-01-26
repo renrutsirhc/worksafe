1a2fae3cfce78195e7ae9822fc9f7409d3c5a3b6
shingteha <Tahiti2021>
2022-01-27 11:39:00 +1300

    Testing changes changed footer

--- WorkSafe_FE/src/components/footer.js
+++ WorkSafe_FE/src/components/footer.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Container, Row, Col } from 'react-bootstrap'
 
 const Footer = () => (
       <footer className="bg-light p-3 text-center">
         <div className="logo" />
         <hr></hr>
         <p>
-          I am footer
+            <Col className='text-center py-3'>Copyright &copy; WorkSafe New Zealand</Col>
         </p>
       </footer>
     );
