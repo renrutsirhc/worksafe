9b94b78a423460346e434d5766647e7633dd6d5c
shingteha <Tahiti2021>
2022-01-29 16:51:42 +1300

    View Entry Component

--- WorkSafe_FE/src/views/view-entry.js
+++ WorkSafe_FE/src/views/view-entry.js
@@ -7,46 +7,14 @@ const ViewEntry = (props) => {
     return (
         <div className="view-entry">
             <Card>
-                <Card.Header as="h5">{props.entry.Description}</Card.Header>
+                <Card.Header as="h4">Title</Card.Header>
                 <Card.Body>
-                    <Card.Title>New Entry</Card.Title>
+                    <Card.Title>{props.entry.Description}y</Card.Title>
                     <Card.Text>
-                        Enter the details below:
                     </Card.Text>
-                    <>
-                        <InputGroup className="mb-3">
-                            <DropdownButton
-                                variant="outline-secondary"
-                                title="Date Picker"
-                                id="input-group-dropdown-1"
-                            >
-                                <Dropdown.Item href="#">Date 1</Dropdown.Item>
-                                <Dropdown.Item href="#">Date 2</Dropdown.Item>
-                                <Dropdown.Item href="#">Date 3</Dropdown.Item>
-                            </DropdownButton>
-                        </InputGroup>
-                        <FloatingLabel controlId="floatingTextarea" label="Title" className="mb-3">
-                            <Form.Control as="textarea" placeholder="Project title" />
-                        </FloatingLabel>
-                        <FloatingLabel controlId="floatingTextarea2" label="Description">
-                            <Form.Control
-                                as="textarea"
-                                placeholder="Description of the project"
-                                style={{ height: '100px' }}
-                            />
-                        </FloatingLabel>
-                        <Form.Select aria-label="Default select example">
-                            <option>Pillers picker</option>
-                            <option value="1">Embedding</option>
-                            <option value="2">Resources</option>
-                            <option value="3">Needs</option>
-                            <option value="4">Leadership</option>
-                            <option value="5">Connection</option>
-                        </Form.Select>
-                    </>
-                    <Button variant="primary">Save Entry</Button>
+                    <Button variant="primary">View Entry</Button>
                 </Card.Body>
-                <Card.Footer as="h5">Card Footer</Card.Footer>
+                <Card.Footer as="h7">Last Updated {new Date().getFullYear()}</Card.Footer>
             </Card>
         </div>
     )
