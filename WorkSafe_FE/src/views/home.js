fba8d4de060b50e0c4fa5777a5cdbfb8cd0adc74
shingteha <Tahiti2021>
2022-01-29 16:18:35 +1300

    Improved entry card

--- WorkSafe_FE/src/views/home.js
+++ WorkSafe_FE/src/views/home.js
@@ -9,57 +9,31 @@ class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            forecasts: [],
-            loading: true,
             entries: [],
         };
         
     }
 
     componentDidMount() {
-        //this.populateWeatherData();
         this.getEntries();
     }
 
-    static renderForecastsTable(forecasts) {
-        return (
-            <table className='table table-striped' aria-labelledby="tabelLabel">
-                <thead>
-                    <tr>
-                        <th>Date</th>
-                        <th>Temp. (C)</th>
-                        <th>Temp. (F)</th>
-                        <th>Summary</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {forecasts.map(forecast =>
-                        <tr key={forecast.Date}>
-                            <td>{forecast.Date}</td>
-                            <td>{forecast.TemperatureC}</td>
-                            <td>{forecast.TemperatureF}</td>
-                            <td>{forecast.Summary}</td>
-                        </tr>
-                    )}
-                </tbody>
-            </table>
-        );
-    }
 
     render() {
+        if (this.state.entries.length > 0) {
+            return (
+                <div>
+                    <ViewEntry entry={this.state.entries[0]} />
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <h2> No Entries to display</h2>
+                </div>
+            );
+        };
 
-        //let contents = this.state.loading
-            //? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-            //: Home.renderForecastsTable(this.state.forecasts);
-
-        return (
-            <div>
-                {/*<h1 id="tabelLabel" >Weather forecast</h1>*/}
-                {/*<p>This component demonstrates fetching data from the server.</p>*/}
-                {/*{contents}*/}
-                <ViewEntry entry="{entries[0]}"/>
-            </div>
-        );
     }
 
     async getEntries() {
@@ -69,11 +43,6 @@ class Home extends Component {
         this.setState({ entries: data });
     }
 
-    async populateWeatherData() {
-        const response = await fetch('https://localhost:7001/weatherforecast');
-        const data = await response.json();
-        this.setState({ forecasts: data, loading: false });
-    }
 }
 
 export default Home;
--- WorkSafe_FE/src/views/view-entry.js
+++ WorkSafe_FE/src/views/view-entry.js
@@ -1,19 +1,50 @@
 import React, { useState } from "react";
 import { ListGroup } from "reactstrap";
-import { Card, Button } from "react-bootstrap"
+import { Card, Button, Form, FloatingLabel, Dropdown, FormControl, DropdownButton, InputGroup } from "react-bootstrap"
 
 const ViewEntry = (props) => {
 
     return (
         <div className="view-entry">
             <Card>
-                <Card.Header as="h5">{props.Title}</Card.Header>
+                <Card.Header as="h5">{props.entry.Description}</Card.Header>
                 <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
+                    <Card.Title>New Entry</Card.Title>
                     <Card.Text>
-                        Card entry data here the card's content.
+                        Enter the details below:
                     </Card.Text>
-                    <Button variant="primary">Explore</Button>
+                    <>
+                        <InputGroup className="mb-3">
+                            <DropdownButton
+                                variant="outline-secondary"
+                                title="Date Picker"
+                                id="input-group-dropdown-1"
+                            >
+                                <Dropdown.Item href="#">Date 1</Dropdown.Item>
+                                <Dropdown.Item href="#">Date 2</Dropdown.Item>
+                                <Dropdown.Item href="#">Date 3</Dropdown.Item>
+                            </DropdownButton>
+                        </InputGroup>
+                        <FloatingLabel controlId="floatingTextarea" label="Title" className="mb-3">
+                            <Form.Control as="textarea" placeholder="Project title" />
+                        </FloatingLabel>
+                        <FloatingLabel controlId="floatingTextarea2" label="Description">
+                            <Form.Control
+                                as="textarea"
+                                placeholder="Description of the project"
+                                style={{ height: '100px' }}
+                            />
+                        </FloatingLabel>
+                        <Form.Select aria-label="Default select example">
+                            <option>Pillers picker</option>
+                            <option value="1">Embedding</option>
+                            <option value="2">Resources</option>
+                            <option value="3">Needs</option>
+                            <option value="4">Leadership</option>
+                            <option value="5">Connection</option>
+                        </Form.Select>
+                    </>
+                    <Button variant="primary">Save Entry</Button>
                 </Card.Body>
                 <Card.Footer as="h5">Card Footer</Card.Footer>
             </Card>
