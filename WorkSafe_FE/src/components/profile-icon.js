844f1edadef212343e145bc03537c98e911125e1
MiLLeRRain <xxdezz@gmail.com>
2022-01-31 11:50:32 +1300

    profile avatar update

--- WorkSafe_FE/src/components/profile-icon.js
+++ WorkSafe_FE/src/components/profile-icon.js
@@ -1,41 +1,22 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 
 import { useAuth0 } from "@auth0/auth0-react";
 import { Avatar } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 import Image from "react-bootstrap/Image";
 
-// const ProfileIcon = () => {
-//   const { isAuthenticated } = useAuth0();
-
-//   return isAuthenticated ? (
-//     <div className="rounded-circle border d-flex justify-content-center align-items-center">
-//       <img
-//         src={picture}
-//         alt="Profile"
-//         className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
-//       />
-//     </div>
-//   ) : (
-//     <div className="rounded-circle border d-flex justify-content-center align-items-center">
-//       Not logged in
-//     </div>
-//   );
-// };
-
-// export default ProfileIcon;
-
 const ProfileIcon = () => {
   const { user, isAuthenticated } = useAuth0();
-  {
-    if (isAuthenticated) {
-      const { picture } = user;
-    }
-  }
 
   console.log(user);
 
   return isAuthenticated ? (
+    <NavLink
+      to="/profile"
+      exact
+      activeClassName="router-link-exact-active"
+    >
     <div className="avatar-icon">
       <Avatar
         src={
@@ -48,6 +29,7 @@ const ProfileIcon = () => {
         size={40}
       />
     </div>
+    </NavLink>
   ) : (
     <div className="avatar-icon">
       <Avatar size={40} icon={<UserOutlined />} />
