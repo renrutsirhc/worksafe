464350a58f066d1fb30024854dfeaa813a968a59
shingteha <Tahiti2021>
2022-01-30 16:53:16 +1300

    Improved entry card UI

--- WorkSafe_FE/src/views/view-entry.js
+++ WorkSafe_FE/src/views/view-entry.js
@@ -1,20 +1,23 @@
-import React, { useState } from "react";
-import { ListGroup } from "reactstrap";
-import { Card, Button, Form, FloatingLabel, Dropdown, FormControl, DropdownButton, InputGroup } from "react-bootstrap"
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Card, Button } from "react-bootstrap"
 
 const ViewEntry = (props) => {
 
     return (
         <div className="view-entry">
             <Card>
-                <Card.Header as="h4">{props.entry.Description}</Card.Header>
+                <Card.Header style={{ backgroundColor: '#ffcc80' }} as="h4">{props.entry.Description}</Card.Header>
                 <Card.Body>
-                    <Card.Title>Card title</Card.Title>
                     <Card.Text> Card properties
                     </Card.Text>
-                    <Button variant="primary">View Entry</Button>
+                    <Button variant="light">View Entry</Button>
+                    <FontAwesomeIcon icon="fa-regular fa-floppy-disk" />
+                    <FontAwesomeIcon icon="fa-regular fa-plus" />
+                    <FontAwesomeIcon icon="fa-regular fa-pen-to-square" />
+                    <FontAwesomeIcon icon="faCoffee" />
                 </Card.Body>
-                <Card.Footer as="h7">Last Updated {new Date().getFullYear()}</Card.Footer>
+                <Card.Footer style={{ backgroundColor: '#fff3e0' }} as="h7">Last Updated {props.entry.TimeStamp} by {props.entry.Id}</Card.Footer>
             </Card>
         </div>
     )
