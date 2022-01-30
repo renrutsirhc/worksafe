3818a70542f894fa0c26723f15becac3131debfd
MiLLeRRain <xxdezz@gmail.com>
2022-01-30 17:40:43 +1300

    icons

--- WorkSafe_FE/src/components/footer.js
+++ WorkSafe_FE/src/components/footer.js
@@ -1,15 +1,16 @@
 import React from "react";
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col } from "react-bootstrap";
 
 const Footer = () => (
-      <footer className="bg-light p-3 text-center">
-        <div className="logo" />
-        <hr></hr>
-        <p>
-            <Col className='text-center py-3'>Copyright &copy; WorkSafe NZ {new Date().getFullYear()}</Col>
-            <Col className='text-center py-1'>Powered by Team Awesomeness</Col>
-        </p>
-      </footer>
-    );
+  <footer className="bg-light p-3 text-center">
+    <div className="logo">
+      <hr></hr>
+      <Col className="text-center py-3">
+        Copyright &copy; WorkSafe NZ {new Date().getFullYear()}
+      </Col>
+      <Col className="text-center py-1">Powered by Team Awesomeness</Col>
+    </div>
+  </footer>
+);
 
 export default Footer;
--- WorkSafe_FE/src/views/view-entry.js
+++ WorkSafe_FE/src/views/view-entry.js
@@ -1,26 +1,34 @@
 import React, { useState, useEffect } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Card, Button } from "react-bootstrap"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFloppyDisk,
+  faPlus,
+  faPenToSquare,
+  faCoffee,
+} from "@fortawesome/free-solid-svg-icons";
+import { Card, Button } from "react-bootstrap";
 
 const ViewEntry = (props) => {
-
-    return (
-        <div className="view-entry">
-            <Card>
-                <Card.Header style={{ backgroundColor: '#ffcc80' }} as="h4">{props.entry.Description}</Card.Header>
-                <Card.Body>
-                    <Card.Text> Card properties
-                    </Card.Text>
-                    <Button variant="light">View Entry</Button>
-                    <FontAwesomeIcon icon="fa-regular fa-floppy-disk" />
-                    <FontAwesomeIcon icon="fa-regular fa-plus" />
-                    <FontAwesomeIcon icon="fa-regular fa-pen-to-square" />
-                    <FontAwesomeIcon icon="faCoffee" />
-                </Card.Body>
-                <Card.Footer style={{ backgroundColor: '#fff3e0' }} as="h7">Last Updated {props.entry.TimeStamp} by {props.entry.Id}</Card.Footer>
-            </Card>
-        </div>
-    )
+  return (
+    <div className="view-entry">
+      <Card>
+        <Card.Header style={{ backgroundColor: "#ffcc80" }} as="h4">
+          {props.entry.Description}
+        </Card.Header>
+        <Card.Body>
+          <Card.Text> Card properties</Card.Text>
+          <Button variant="light">View Entry</Button>
+          <FontAwesomeIcon icon={faFloppyDisk} />
+          <FontAwesomeIcon icon={faPlus} />
+          <FontAwesomeIcon icon={faPenToSquare} />
+          <FontAwesomeIcon icon={faCoffee} />
+        </Card.Body>
+        <Card.Footer style={{ backgroundColor: "#fff3e0" }} as="h5">
+          Last Updated {props.entry.TimeStamp} by {props.entry.Id}
+        </Card.Footer>
+      </Card>
+    </div>
+  );
 };
 
 export default ViewEntry;
