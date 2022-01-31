a32a91061e47d1c395fa6321572a3708ed220bb8
shingteha <Tahiti2021>
2022-01-31 15:34:22 +1300

    Fixed time formatting

--- WorkSafe_FE/src/views/view-entry.js
+++ WorkSafe_FE/src/views/view-entry.js
@@ -1,31 +1,32 @@
-import React, { useState, useEffect } from "react";
+import React, { Component, useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import { Card, Button } from "react-bootstrap";
 import "../styles/view-entry.css";
+import Moment from 'react-moment';
+import 'moment-timezone';
 
+// Display view-entry card
 const ViewEntry = (props) => {
-  console.log(props.entry);
-  return (
-    <div className="view-entry">
-      <Card>
-        <Card.Header style={{ backgroundColor: "#ffcc80" }} as="h4">
-          {props.entry.Description}
-          <div className="EditButton">
-            <Button variant="light">
-              <FontAwesomeIcon icon={faPenToSquare} />
-            </Button>
-          </div>
-        </Card.Header>
-        <Card.Body>
-          <Card.Text> Card properties</Card.Text>
-        </Card.Body>
-        <Card.Footer style={{ backgroundColor: "#fff3e0" }} as="h6">
-          Last Updated {props.entry.TimeStamp} by {props.entry.Id}
-        </Card.Footer>
-      </Card>
-    </div>
-  );
+
+    return (
+        <div className="view-entry">
+            <Card>
+                <Card.Header style={{ backgroundColor: "#ffcc80" }} as="h4">
+                    {props.entry.Title}
+                    <div className="EditButton">
+                        <Button variant="light"><FontAwesomeIcon icon={faPenToSquare} /></Button>
+                    </div>
+                </Card.Header>
+                <Card.Body>
+                    <Card.Text> {props.entry.Description}</Card.Text>
+                </Card.Body>
+                <Card.Footer style={{ backgroundColor: "#fff3e0" }} as="h6">
+                    Last Updated <Moment format="HH:MM, D MMM YYYY" withTitle>{props.entry.TimeStamp}</Moment> by {props.entry.Author.Name}
+                </Card.Footer>
+            </Card>
+        </div>
+    );
 };
 
 export default ViewEntry;
