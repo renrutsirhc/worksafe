9fe89561c5953ad85c874c552711275847c9c23d
MiLLeRRain <xxdezz@gmail.com>
2022-02-01 15:55:49 +1300

    added selector to add entry

--- WorkSafe_FE/src/components/entry-parent.js
+++ WorkSafe_FE/src/components/entry-parent.js
@@ -3,58 +3,65 @@ import { Component } from "react";
 //import EditEntry from "../Views/edit-entry";
 
 class EntryParent extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            Editing: false,
-            Expanded: false,
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      Editing: false,
+      Expanded: false,
+    };
 
-        this.setEditing = this.setEditing.bind(this);
-        this.setExpanded = this.setExpanded.bind(this);
+    this.setEditing = this.setEditing.bind(this);
+    this.setExpanded = this.setExpanded.bind(this);
+  }
+
+  setEditing() {
+    if (this.state.Editing) {
+      this.setState({ Editing: false });
+    } else {
+      this.setState({
+        Editing: true,
+        Expanded: false,
+      });
+    }
+  }
+
+  setExpanded() {
+    if (this.state.Expanded) {
+      this.setState({ Expanded: false });
+    } else {
+      this.setState({ Expanded: true });
+    }
+  }
+
+  render() {
+    if (this.state.Editing) {
+      return (
+        <EditEntry
+          entry={this.props.entry}
+          projects={this.props.projects}
+          setEditing={this.setEditing}
+          handleUpdateEntry={this.props.handleUpdateEntry}
+        />
+      );
+    }
+    if (this.state.Expanded) {
+      return (
+        <ViewFullEntry
+          entry={this.props.entry}
+          setExpanded={this.setExpanded}
+          setEditing={this.setEditing}
+        />
+      );
     }
 
-    setEditing() {
-        if (this.state.Editing) {
-            this.setState({ Editing: false })
-        }
-        else {
-            this.setState({
-                Editing: true,
-                Expanded: false
-            })
-        }
-    }
-
-    setExpanded() {
-        if (this.state.Expanded) {
-            this.setState({ Expanded: false })
-        }
-        else {
-            this.setState({ Expanded: true })
-        }
-    }
-
-    render() {
-        if (this.state.Editing) {
-            return (
-                <EditEntry entry={this.props.entry} setEditing={this.setEditing} handleUpdateEntry={this.props.handleUpdateEntry}/>
-            )
-        }
-        if (this.state.Expanded) {
-            return (
-                <ViewFullEntry entry={this.props.entry} setExpanded={this.setExpanded} setEditing={this.setEditing} />
-            )
-        }
-
-        return (
-            <ViewEntry entry={this.props.entry} setExpanded={this.setExpanded} setEditing={this.setEditing} />
-        )
-    }
-
-
-
+    return (
+      <ViewEntry
+        entry={this.props.entry}
+        setExpanded={this.setExpanded}
+        setEditing={this.setEditing}
+      />
+    );
+  }
 }
-
 
 export default EntryParent;
--- WorkSafe_FE/src/views/dashboard.js
+++ WorkSafe_FE/src/views/dashboard.js
@@ -7,117 +7,121 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 import Moment from "moment";
 import Select from "react-dropdown-select";
 import { Button } from "react-bootstrap";
-import AddEntry from "./add-entry.js"
-import EntryParent from "../components/entry-parent.js"
+import AddEntry from "./add-entry.js";
+import EntryParent from "../components/entry-parent.js";
 import { withAuth0 } from "@auth0/auth0-react";
 
 class Dashboard extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            loading: true,
-            entries: [],
-            projects: [],
-            addEntry: false,
-            user: props.auth0.user,
-        }
+    this.state = {
+      loading: true,
+      projectsLoaded: false,
+      entries: [],
+      projects: [],
+      addEntry: false,
+      user: props.auth0.user,
+    };
 
-        this.handleAddEntry = this.handleAddEntry.bind(this);
-        this.handleUpdateEntry = this.handleUpdateEntry.bind(this);
+    this.handleAddEntry = this.handleAddEntry.bind(this);
+    this.handleUpdateEntry = this.handleUpdateEntry.bind(this);
+  }
 
-       
+  componentDidMount() {
+    this.getProjects();
+    this.getEntries();
+  }
+
+  async getProjects() {
+    let result = await fetch("/api/projects");
+    let data = await result.json();
+    this.setState({ projects: data, projectsLoaded: true });
+  }
+
+  async getEntries() {
+    let result = await fetch("/api/users/" + this.state.user.sub + "/entries");
+    let data = await result.json();
+    this.setState({ entries: data });
+    this.setState({ loading: false });
+  }
+
+  handleAddEntry() {
+    this.setState({ addEntry: true });
+  }
+
+  handleUpdateEntry(entry) {
+    var entries = this.state.entries;
+    for (let i = 0; i < entries.length; i++) {
+      if (entries[i].Id == entry.Id) {
+        entries[i] = entry;
+        this.setState({ entries: entries });
+        break;
+      }
+    }
+  }
+
+  render() {
+    var projects = this.state.projects;
+    class ProjectOption {
+      constructor(label, value) {
+        this.label = label;
+        this.value = value;
+      }
+    }
+    var projectOptions = [];
+    projects.map(function (project, index) {
+      const po = new ProjectOption(project.Title, project.Id);
+      projectOptions[index] = po;
+    });
+    var entries = this.state.entries.map((entry) => (
+      <EntryParent
+        key={entry.Id}
+        entry={entry}
+        projects={this.state.projects}
+        handleUpdateEntry={this.handleUpdateEntry}
+      />
+    ));
+
+    if (this.state.loading) {
+      return (
+        <div>
+          <h2>Loading...</h2>
+        </div>
+      );
     }
 
-    componentDidMount() {
-        this.getProjects();
-        this.getEntries();
+    if (this.state.addEntry) {
+      return <AddEntry userId={this.state.user.sub} />;
     }
 
-    async getProjects() {
-        let result = await fetch("/api/projects");
-        let data = await result.json();
-        this.setState({ projects: data });
-    }
-
-    async getEntries() {
-        let result = await fetch("/api/users/" + this.state.user.sub + "/entries");
-        let data = await result.json();
-        this.setState({ entries: data });
-        this.setState({ loading: false });
-    }
-
-    handleAddEntry() {
-        this.setState({ addEntry: true })
-    }
-
-    handleUpdateEntry(entry) {
-        var entries = this.state.entries;
-        for (let i = 0; i < entries.length; i++) {
-            if (entries[i].Id == entry.Id) {              
-                entries[i] = entry;
-                this.setState({ entries: entries });
-                break;
-            }
-        }
-    }
-
-
-    render() {
-        var projects = this.state.projects;
-        class ProjectOption {
-            constructor(label, value) {
-                this.label = label;
-                this.value = value;
-            }
-        }
-        var projectOptions = [];
-        projects.map(function (project, index) {
-            const po = new ProjectOption(project.Title, project.Id);
-            projectOptions[index] = po;
-        });
-        var entries = this.state.entries.map(entry => (
-            <EntryParent key={entry.Id} entry={entry} handleUpdateEntry={this.handleUpdateEntry} />
-        ));
-
-        if (this.state.loading) {
-            return (
-                <div>
-                    <h2>Loading...</h2>
-                </div>
-            );
-        }
-
-        if (this.state.addEntry) {
-            return (
-                <AddEntry userId={this.state.user.sub} />
-            )
-        }
-
-        if (this.state.entries.length > 0) {
-            return (
-                <div>
-                    <Button variant="success" onClick={this.handleAddEntry}>Add Entry</Button>
-                    <div className="list-group">
-                        <div className="d-flex">
-                            <div className="mr-auto">
-                                <h2>Feed</h2>
-                            </div>
-                        </div>
-                        {entries}
-                    </div>
-                </div>
-
-            );
-        }
-
-        return (
-            <div>
-                <Button variant="success" onClick={this.handleAddEntry}>Add Entry</Button>
-                <h2>No Entries to Display...</h2>
+    if (this.state.entries.length > 0 && this.state.projectsLoaded == true) {
+      return (
+        <div>
+          <Button variant="success" onClick={this.handleAddEntry}>
+            Add Entry
+          </Button>
+          <div className="list-group">
+            <div className="d-flex">
+              <div className="mr-auto">
+                <h2>Feed</h2>
+              </div>
             </div>
-        );
+            {entries}
+          </div>
+        </div>
+      );
     }
+
+    return (
+      <div>
+        <Button variant="success" onClick={this.handleAddEntry}>
+          Add Entry
+        </Button>
+        <h2>No Entries to Display...</h2>
+      </div>
+    );
+  }
 }
 
 export default withAuth0(Dashboard);
