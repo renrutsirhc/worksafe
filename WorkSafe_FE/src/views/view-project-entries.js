81ef530bd5c51d98eea2f94238fc2ba8d15de5ad
shingteha <Tahiti2021>
2022-02-08 16:04:27 +1300

    Fixed Title errors

--- WorkSafe_FE/src/views/view-project-entries.js
+++ WorkSafe_FE/src/views/view-project-entries.js
@@ -2,23 +2,61 @@ import React, { Component } from "react"
 import { Card } from "react-bootstrap"
 import "../styles/styles.css"
 import { CardHeaderWithEditButton, CardFooter } from "../components"
+import { ViewFullProject } from "../views"
 
-// Display view-project-entry card
+// Display view-project-entries
 class ViewProjectEntries extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      Project: this.props.project,
+      entries: []
+    }
+  }
+
+  async getEntriesByProject() {
+    const { getAccessTokenSilently } = this.props.auth0
+    var token = await getAccessTokenSilently()
+    var options = {
+      headers: {
+        Authorization: "Bearer " + token
+      }
+    }
+    let response = await fetch("/api/projects/" + this.state.project.id + "/entries", options)
+    if (response.ok) {
+      let result = await response.json()
+      this.setState({ projects: result, projectsLoaded: true })
+    } else {
+      //error
+    }
   }
 
   render() {
-    return (
-      <div className="view-entry mt-3">
-        <Card className="grow" onClick={this.props.setExpanded}>
-          <CardHeaderWithEditButton title={this.props.project.Title} subTitle={this.props.project.Title} />
-          <Card.Body className="card-body">
-            <Card.Text> {this.props.project.Description}</Card.Text>
-          </Card.Body>
+    var entries = this.state.entries.map(entry => <EntryParent key={entry.Id} entry={entry} projects={projects} tags={tags} handleUpdateEntry={this.handleUpdateEntry} />)
 
-        </Card>
+    if (this.state.entries.length > 0 && this.state.projectsLoaded) {
+      return (
+        <div>
+          <ViewFullProject project={this.props.project} />
+          <div className="list-group">
+            <div className="d-flex">
+              <div className="mx-auto"></div>
+            </div>
+            {entries}
+          </div>
+        </div>
+      )
+    }
+
+    return (
+      <div>
+        <ViewFullProject project={this.props.project} />
+        <div className="d-flex">
+          <div className="mx-auto"></div>
+          <div className="ml-auto"></div>
+        </div>
+
+        <h2>No Entries to Display...</h2>
       </div>
     )
   }
