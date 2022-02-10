63f120328b1b2866351afe47617904817c81812d
shingteha <Tahiti2021>
2022-02-10 14:44:34 +1300

    Added CSS file

--- WorkSafe_FE/src/reports/user-report-body.js
+++ WorkSafe_FE/src/reports/user-report-body.js
@@ -1,94 +1,49 @@
-import React from "react";
+import React from "react"
+import "../styles/report-styles.css"
 
-const UserReportBody = (props) => {
+const UserReportBody = props => {
   return (
     <table width="100%" cellSpacing={0} cellPadding={0} bgcolor="#f6f9e1">
       <tbody>
         <tr>
-          <td
-            style={{ background: "#648181" }}
-            colSpan={2}
-            valign="top"
-            bgcolor="#648181"
-            width="100%"
-            height={22}
-          >
+          <td className="report-body-title" colSpan={2} valign="top" bgcolor="#e1e5da" width="100%" height={22}>
             <h2 className="western" lang="en-GB">
               I updated some things Great Project
             </h2>
           </td>
         </tr>
         <tr>
-          <td
-            style={{ background: "#e1e5da" }}
-            colSpan={2}
-            valign="top"
-            bgcolor="#e1e5da"
-            width="100%"
-            height={22}
-          >
+          <td style={{ background: "#e1e5da" }} colSpan={2} valign="top" bgcolor="#e1e5da" width="100%" height={22}>
             <p className="western" lang="en-GB">
               <span style={{ color: "#ffffff" }}>
                 <strong>Description</strong>
               </span>
             </p>
-            <p lang="en-GB">
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas
-              porttitor congue massa. Fusce posuere, magna sed pulvinar
-              ultricies, purus lectus malesuada libero, sit amet commodo magna
-              eros quis urna.
-            </p>
+            <p lang="en-GB">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.</p>
           </td>
         </tr>
         <tr>
-          <td
-            style={{ background: "#e1e5da" }}
-            colSpan={2}
-            valign="top"
-            bgcolor="#e1e5da"
-            width="100%"
-            height={22}
-          >
+          <td style={{ background: "#e1e5da" }} colSpan={2} valign="top" bgcolor="#e1e5da" width="100%" height={22}>
             <p className="western" lang="en-GB">
               <span style={{ color: "#ffffff" }}>
                 <strong>Learning</strong>
               </span>
             </p>
-            <p lang="en-GB">
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas
-              porttitor congue massa.
-            </p>
+            <p lang="en-GB">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa.</p>
           </td>
         </tr>
         <tr>
-          <td
-            style={{ background: "#e1e5da" }}
-            colSpan={2}
-            valign="top"
-            bgcolor="#e1e5da"
-            width="100%"
-            height={22}
-          >
+          <td style={{ background: "#e1e5da" }} colSpan={2} valign="top" bgcolor="#e1e5da" width="100%" height={22}>
             <p className="western" lang="en-GB">
               <span style={{ color: "#ffffff" }}>
                 <strong>Impact</strong>
               </span>
             </p>
-            <p lang="en-GB">
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas
-              porttitor congue massa. Fusce posuere, magna sed pulvinar
-              ultricies, purus lectus malesuada libero, sit amet commodo magna
-              eros quis urna. Nunc viverra imperdiet enim.
-            </p>
+            <p lang="en-GB">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim.</p>
           </td>
         </tr>
         <tr valign="top">
-          <td
-            style={{ background: "#e1e5da" }}
-            bgcolor="#e1e5da"
-            width="28%"
-            height={22}
-          >
+          <td style={{ background: "#e1e5da" }} bgcolor="#e1e5da" width="28%" height={22}>
             <p className="western" lang="en-GB">
               <span style={{ color: "#ffffff" }}>
                 <strong>Mindset</strong>
@@ -102,23 +57,13 @@ const UserReportBody = (props) => {
                 <strong>Next Steps</strong>
               </span>
             </p>
-            <p lang="en-GB">
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-            </p>
+            <p lang="en-GB">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
             <p lang="en-GB">Maecenas porttitor congue massa.</p>
-            <p lang="en-GB">
-              Fusce posuere, magna sed pulvinar ultricies, purus lectus
-              malesuada libero, sit amet commodo magna eros quis urna.
-            </p>
+            <p lang="en-GB">Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.</p>
           </td>
         </tr>
         <tr valign="top">
-          <td
-            style={{ background: "#e1e5da" }}
-            bgcolor="#e1e5da"
-            width="28%"
-            height={22}
-          >
+          <td style={{ background: "#e1e5da" }} bgcolor="#e1e5da" width="28%" height={22}>
             <p className="western" lang="en-GB">
               <span style={{ color: "#ffffff" }}>
                 <strong>Tags</strong>
@@ -138,14 +83,7 @@ const UserReportBody = (props) => {
           </td>
         </tr>
         <tr>
-          <td
-            style={{ background: "#648181" }}
-            colSpan={2}
-            valign="top"
-            bgcolor="#648181"
-            width="100%"
-            height={22}
-          >
+          <td style={{ background: "#648181" }} colSpan={2} valign="top" bgcolor="#648181" width="100%" height={22}>
             <p className="western" lang="en-GB">
               <span style={{ color: "#ffffff" }}>
                 <strong>Freddie </strong>22/01/22
@@ -155,7 +93,7 @@ const UserReportBody = (props) => {
         </tr>
       </tbody>
     </table>
-  );
-};
+  )
+}
 
-export default UserReportBody;
+export default UserReportBody
