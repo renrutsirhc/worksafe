061b9322621fa4ba8a5579621def37b177b02622
Selina <yutong@myvuw.ac.nz>
2022-02-15 14:42:20 +1300

    add back btn

--- WorkSafe_FE/src/views/view-project-entries.js
+++ WorkSafe_FE/src/views/view-project-entries.js
@@ -9,7 +9,7 @@ import {
 import { ViewFullProject, AddEntry } from "./";
 import { withAuth0 } from "@auth0/auth0-react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { DeleteConfirmationModal } from "../components";
 
 // Display view-project-entries
@@ -201,7 +201,15 @@ class ViewProjectEntries extends Component {
                         <h6>Goal: {this.props.project.ProjectGoal}</h6>
                         <h6>Pillars: {pillars}</h6>
                         <div className="d-flex">
-                            <div className="mr-auto"></div>
+                            <div className="mr-auto">
+                                {/*add back btn*/}
+                                <a
+                                    href="Projects"
+                                    className="button round-button"
+                                >
+                                    <FontAwesomeIcon icon={faArrowLeft} />
+                                </a>
+                            </div>
                             <div className="mx-auto"></div>
                             <div className="ml-auto">
                                 <button
@@ -235,6 +243,15 @@ class ViewProjectEntries extends Component {
                     <h6>Pillars: {pillars}</h6>
                 </div>
                 <div className="d-flex">
+                    <div className="mr-auto">
+                        {/*still show back btn when there's no entry to display*/}
+                        <a
+                            href="Projects"
+                            className="button round-button"
+                        >
+                            <FontAwesomeIcon icon={faArrowLeft} />
+                        </a>
+                    </div>
                     <div className="mx-auto"></div>
                     <div className="ml-auto"></div>
                 </div>
